4cf25d7ec1530da1a524952aa35eaa724cb03eb8
Dmitry Kirillov <mr.lurix@gmail.com>
2025-12-16 13:20:31 +0300

    chore update path

--- lab5/lab5WithMutex/run_two_instances.bat
+++ lab5/lab5WithMutex/run_two_instances.bat
@@ -1,3 +1,3 @@
 @echo off
-start "" "C:\Users\mrlur\Documents\study\PP\lab5\lab5\x64\Debug\lab5.exe"
-start "" "C:\Users\mrlur\Documents\study\PP\lab5\lab5\x64\Debug\lab5.exe"
+start "" "C:\Users\mrlur\Documents\study\PP\lab5\lab5WithMutex\x64\Debug\lab5WithMutex.exe"
+start "" "C:\Users\mrlur\Documents\study\PP\lab5\lab5WithMutex\x64\Debug\lab5WithMutex.exe"
