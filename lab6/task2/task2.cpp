a382ece481acccf5fef25ae1e9658466f7356c4f
Dmitry Kirillov <mr.lurix@gmail.com>
2025-12-21 23:59:19 +0300

    fix

--- lab6/task2/task2.cpp
+++ lab6/task2/task2.cpp
@@ -4,8 +4,9 @@
 int main()
 {
     int x = 44;
-    #pragma omp parallel for private(x)
-    for (int i = 0; i <= 10; i++) {
+#pragma omp parallel for private(x)
+    for (int i = 0; i <= 10; i++)
+    {
         x = i;
         printf("Thread number: %d x: %d\n", omp_get_thread_num(), x);
     }
